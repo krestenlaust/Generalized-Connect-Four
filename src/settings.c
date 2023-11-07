616b7a356b8d17d837395bd357bd0d15f8f73433
Kresten Laust <krestenlaust@gmail.com>
2023-11-07 21:16:22 +0100

    Corrected formatting

--- src/settings.c
+++ src/settings.c
@@ -2,9 +2,9 @@
 
 char* format_settings(struct game_settings settings){
     // Rough estimate of string length.
-    char* settings_string = calloc(sizeof(char) * 45);
+    char* settings_string = calloc(45, sizeof(char));
 
-    sprintf(settings_string, "AI opponent:\t%d\nGame size:\t%d*%d\nLine size:\t%d",
+    sprintf(settings_string, "AI opponent:\t%d\nBoard size:\t%d X %d\nLine size:\t%d",
             settings.ai_opponent,
             settings.game_size_width,
             settings.game_size_height,
