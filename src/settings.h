73f2dfcf4726338757247d1a9602a3be99c21139
Kresten Laust <krestenlaust@gmail.com>
2023-11-07 20:45:35 +0100

    Added procedure signature

--- src/settings.h
+++ src/settings.h
@@ -1,7 +1,16 @@
 #include "stdbool.h"
 
+/**
+ * This struct is only used by play_game to represent player input.
+ */
 struct game_settings {
     bool ai_opponent;
-    int game_size;
+    int game_size_width;
+    int game_size_height;
     int line_size;
 };
+
+struct game_settings get_settings_scan_console(void);
+// Examples of expansion
+//struct game_settings get_settings_args(void);
+//struct game_settings get_settings_file(void);
