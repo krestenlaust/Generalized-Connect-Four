4fc63c648f6115c09ac66a66bdd4f98cebc9ca09
Kresten Laust <krestenlaust@gmail.com>
2023-11-18 20:13:47 +0100

    Finished console player agent

--- src/player_types.c
+++ src/player_types.c
@@ -1,4 +1,5 @@
 #include "player_types.h"
+#include <stdio.h>
 
 /**
  * Returns a random valid move.
@@ -21,6 +22,24 @@ int player_agent_random_ai(struct game_board board, struct game_rules rules){
             break;
         }
     }
+
+    return move;
+}
+
+int player_agent_console(struct game_board board, struct game_rules rules){
+    printf("Input a number between 1 and %d> ", board.width);
+
+    int move;
+
+    do {
+        int move_one_indexed;
+
+        if (!scanf("%d", &move_one_indexed)){
+            continue;
+        }
+
+        move = move_one_indexed - 1;
+    } while (move < 0 || move >= board.width);
 
     return move;
 }
