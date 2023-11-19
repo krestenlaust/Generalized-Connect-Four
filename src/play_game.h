002ecae9f6d8a26bc4893f5632d39475ccd77336
Kresten Laust <krestenlaust@gmail.com>
2023-11-19 11:26:15 +0100

    Added checker to game loop

--- src/play_game.h
+++ src/play_game.h
@@ -3,6 +3,7 @@
 #include "settings.h"
 #include "game_visualizer.h"
 #include "player_types.h"
+#include "game_checkers.h"
 
 void play_game(get_settings settings_getter);
-int game_of_lines(struct game_board board, struct game_rules rules, player_agent* players, int player_count, game_visualizer visualizer);
+int game_of_lines(struct game_board board, struct game_rules rules, player_agent* players, int player_count, game_visualizer visualizer, game_checker checker);
