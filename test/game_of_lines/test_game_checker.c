124eeb7ac70c92456aa812ed6702fa353757310a
Kresten Laust <krestenlaust@gmail.com>
2023-11-19 11:10:51 +0100

    Added game checker function-signatures and tests

--- test/game_of_lines/test_game_checker.c
+++ test/game_of_lines/test_game_checker.c
@@ -1,5 +1,112 @@
+#include <assert.h>
+#include "game_checkers.h"
+
+void test_nearest_slots_horisontal_win(void);
+void test_nearest_slots_vertical_win(void);
+void test_nearest_slots_empty(void);
+void test_nearest_slots_diagonal_1(void);
+void test_nearest_slots_diagonal_2(void);
 
 // Write unit tests!
 int main(void) {
+    test_nearest_slots_horisontal_win();
+    test_nearest_slots_vertical_win();
+    test_nearest_slots_empty();
+    test_nearest_slots_diagonal_1();
+    test_nearest_slots_diagonal_2();
+}
 
+void assert_game_checker_horisontal_win(game_checker checker){
+    int board_setup[] = {
+            EMPTY_CELL, EMPTY_CELL, EMPTY_CELL, EMPTY_CELL, EMPTY_CELL,
+            EMPTY_CELL, 1, 1, 1, 1,
+            EMPTY_CELL, EMPTY_CELL, EMPTY_CELL, EMPTY_CELL, EMPTY_CELL
+    };
+
+    struct game_board board = {.width = 5, .height = 3, .cells = board_setup};
+    struct game_rules rules = {.line_size = 4};
+
+    int winner = checker(board, rules);
+
+    assert(winner == 1);
+}
+
+void assert_game_checker_vertical_win(game_checker checker){
+    int board_setup[] = {
+            EMPTY_CELL, 1, EMPTY_CELL, EMPTY_CELL, EMPTY_CELL,
+            EMPTY_CELL, 1, EMPTY_CELL, EMPTY_CELL, EMPTY_CELL,
+            EMPTY_CELL, 1, EMPTY_CELL, EMPTY_CELL, EMPTY_CELL
+    };
+
+    struct game_board board = {.width = 5, .height = 3, .cells = board_setup};
+    struct game_rules rules = {.line_size = 3};
+
+    int winner = checker(board, rules);
+
+    assert(winner == 1);
+}
+
+void assert_game_checker_empty(game_checker checker){
+    int board_setup[] = {
+            EMPTY_CELL, EMPTY_CELL, EMPTY_CELL, EMPTY_CELL, EMPTY_CELL,
+            EMPTY_CELL, EMPTY_CELL, EMPTY_CELL, EMPTY_CELL, EMPTY_CELL,
+            EMPTY_CELL, EMPTY_CELL, EMPTY_CELL, EMPTY_CELL, EMPTY_CELL
+    };
+
+    struct game_board board = {.width = 5, .height = 3, .cells = board_setup};
+    struct game_rules rules = {.line_size = 3};
+
+    int winner = checker(board, rules);
+
+    assert(winner == NO_PLAYER);
+}
+
+void assert_game_checker_diagonal_1(game_checker checker){
+    int board_setup[] = {
+            EMPTY_CELL, 1, EMPTY_CELL, EMPTY_CELL, EMPTY_CELL,
+            EMPTY_CELL, EMPTY_CELL, 1, EMPTY_CELL, EMPTY_CELL,
+            EMPTY_CELL, EMPTY_CELL, EMPTY_CELL, 1, EMPTY_CELL
+    };
+
+    struct game_board board = {.width = 5, .height = 3, .cells = board_setup};
+    struct game_rules rules = {.line_size = 3};
+
+    int winner = checker(board, rules);
+
+    assert(winner == 1);
+}
+
+void assert_game_checker_diagonal_2(game_checker checker){
+    int board_setup[] = {
+            EMPTY_CELL, EMPTY_CELL, EMPTY_CELL, 1, EMPTY_CELL,
+            EMPTY_CELL, EMPTY_CELL, 1, EMPTY_CELL, EMPTY_CELL,
+            EMPTY_CELL, 1, EMPTY_CELL, EMPTY_CELL, EMPTY_CELL
+    };
+
+    struct game_board board = {.width = 5, .height = 3, .cells = board_setup};
+    struct game_rules rules = {.line_size = 3};
+
+    int winner = checker(board, rules);
+
+    assert(winner == 1);
+}
+
+void test_nearest_slots_horisontal_win(void){
+    assert_game_checker_horisontal_win(game_checker_nearest_slots);
+}
+
+void test_nearest_slots_vertical_win(void){
+    assert_game_checker_vertical_win(game_checker_nearest_slots);
+}
+
+void test_nearest_slots_empty(void){
+    assert_game_checker_empty(game_checker_nearest_slots);
+}
+
+void test_nearest_slots_diagonal_1(void){
+    assert_game_checker_diagonal_1(game_checker_nearest_slots);
+}
+
+void test_nearest_slots_diagonal_2(void){
+    assert_game_checker_diagonal_2(game_checker_nearest_slots);
 }
